7b68da8e16311c16cb46c4b2ba04148519dd90f4
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2019-11-13 19:22:44 +0300

    Update www.1des.net-chatt-www.te3b.com.ash.js

--- www.1des.net-chatt-www.te3b.com.ash.js
+++ www.1des.net-chatt-www.te3b.com.ash.js
@@ -1,1 +1,46 @@
-$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b> عروض مـُسره للمشتركين / اضغط  هنا </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center"><b><font color="#000080"> عروض خاصه للمشتركين / بمناسبة قرب شهر رمضان المبارك </font></b></p></p><div style="float: right;padding-right: 20px;">-1</div> درع ماستر ذهبي لمدة <font color="#0000FF"> شهرين </font> بـ 1200 ريال/ <font color="#008080"> مع اضافة لايكات وريش </font><img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات وريش </font> بـ 900 مع اللوحه &nbsp;<img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">-2</div> درع ماستر اسود لمدة <font color="#0000FF"> شهرين </font>بـ 1000 ريال/<font color="#008080"> مع اضافة لايكات و ريش </font><img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات و ريش </font> بـ  700  مع اللوحه <img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">-3</div><img src="https://www.te3b.com/s1.gif"> تاج ذهبي <font color="#008080"><u> مخفي </u></font> لمدة <font color="#0000FF"> شهرين </font> بـ 400 ريال + سوبر ذهبي هديه <img src="https://www.te3b.com/sico/z1d63jlqlq10.gif"></p><div style="float: right;padding-right: 20px;">-4</div><img src="https://www.te3b.com/s2.gif"> تاج ملكي اسود <u><font color="#008080"> مخفي </font></u>لمدة<font color="#0000FF"> شهرين </font> بـ 300 ريال + سوبر اسود هديه <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">-5</div><img src="https://www.te3b.com/s3.gif"> تاج فضي لمدة <font color="#0000FF"> شهرين </font> بـ 200 ريال + سوبر فضي هديه &nbsp;<img src="https://www.te3b.com/sico/z1d3c137ha10.gif"></p><div style="float: right;padding-right: 20px;"> - 6</div> تصميم بنر اسم مع التركيب في الشات لمدة <font color="#0000FF">شهرين</font> بـ 100 ريال / <font color="#008080"> او بنرين بـ 150 ريال </font></p><div style="float: right;padding-right: 20px;"> - 7</div> تصميم بنر خلفيه فيديو مع التركيب في الشات لمدة <font color="#0000FF">شهرين</font> بـ 200 ريال / <font color="#008080"> أو بنرين بـ 300 ريال </font><p style="text-align: center"><b><u><font color="#0000FF">ينتهي العرض نهاية شهر شعبان</font></u></font></b><p style="text-align: center"><b> وكل عام و الجميع بالف خيرو عافيه / الاداره </b><p style="text-align: center"><u><b><a target="_blank"href="https://www.te3b.com/sico/z1cj3oqojb10.JPG"> و للتواصل مع الاداره اضغط هنا </a></b></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
+	<span style="background-color: #FFFFFF">
+	<img src="https://www.te3b.com/dro3/z1bru3spme10.gif"></span><font id="sultan" class=" sultan11"><u><b><span lang="ar-ye"><font color="#008000"><span style="background-color: #FFFFFF">
+	</span></font> <span style="background-color: #FFFFFF">
+	<font color="#FF0000">بمناسبة العيد الوطني</font></span></span><span style="background-color: #FFFFFF"><font color="#FF0000"> / اضغط  هنا</font></span> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
+			<b><span style="background-color: #FFFFFF" lang="ar-ye">تخفيضات</span><span style="background-color: #FFFFFF"> خاصه 
+			للمشتركين / بمناسبة قـُرب </span><span lang="ar-ye">
+			<span style="background-color: #FFFFFF">العيد الوطني العماني</span><font color="#000080"> </font>
+			</span></b><img src="https://www.te3b.com/sico/z1dpijftlo10.gif"><b><font color="#000080"><span lang="ar-ye">
+			</span>&nbsp;</font></b></p></p><div style="float: right;padding-right: 20px;">-1</div> درع ماستر ذهبي لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font><span lang="ar-ye">
+			<font face="Ebrima" color="#000000">
+			<span style="vertical-align: middle"><strike>1600</strike></span></font><font color="#0000FF"> </font>
+			الى</span> <font face="Ebrima">1200</font> / <font color="#008080"> مع اضافة لايكات 
+			وريش<span lang="ar-ye"> </span>&nbsp;</font><img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات وريش<span lang="ar-ye">
+			</span> </font> <span lang="ar-ye"><font face="Ebrima"><strike>
+			<font color="#000000"> 1200</font></strike></font></span><font color="#008080"> </font> 
+			<span lang="ar-ye">الى</span> <span lang="ar-ye">
+			<font face="Ebrima">800</font></span> <span lang="ar-ye">/ </span>مع اللوحه &nbsp;<img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">-2</div> درع ماستر اسود لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font><span lang="ar-ye"><strike>
+			<font face="Ebrima" color="#000000">1300</font></strike></span><font color="#0000FF"> </font>
+			<span lang="ar-ye">الى</span> <span lang="ar-ye">
+			<font face="Ebrima">900</font></span> /<font color="#008080"> مع اضافة لايكات و ريش </font><img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات و ريش<span lang="ar-ye">
+			</span> </font> <span lang="ar-ye"><strike>
+			<font face="Ebrima" color="#000000">1000</font></strike></span><font color="#008080"> </font> 
+			<span lang="ar-ye">الى</span> <span lang="ar-ye">
+			<font face="Ebrima">600</font></span> <span lang="ar-ye">/ </span>مع اللوحه <img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">-3</div><img src="https://www.te3b.com/s1.gif"> تاج ذهبي <font color="#008080"><u> مخفي </u></font> لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font> <span lang="ar-ye"><strike>
+			<font face="Ebrima" color="#000000">600</font></strike></span><font color="#0000FF"> </font> 
+			<span lang="ar-ye">الى</span> <font face="Ebrima">400</font><span lang="ar-ye"> 
+			ريال</span> + سوبر ذهبي هديه <img src="https://www.te3b.com/sico/z1d63jlqlq10.gif"></p><div style="float: right;padding-right: 20px;">-4</div><img src="https://www.te3b.com/s2.gif"> تاج ملكي اسود <u><font color="#008080"> مخفي </font></u>لمدة<font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font> <span lang="ar-ye"><strike>
+			<font face="Ebrima" color="#000000">500</font></strike></span><font color="#0000FF"> </font> 
+			<span lang="ar-ye">الى</span> <font face="Ebrima">300</font> ريال + سوبر اسود هديه <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">-5</div><img src="https://www.te3b.com/s3.gif"> تاج فضي لمدة <font color="#0000FF"> شهرين </font> 
+			<strike><font face="Ebrima" color="#000000">400</font></strike><font color="#0000FF"> </font>
+			<span lang="ar-ye">الى</span> <font face="Ebrima">200</font> ريال + سوبر فضي هديه &nbsp;<img src="https://www.te3b.com/sico/z1d3c137ha10.gif"></p><div style="float: right;padding-right: 20px;"> - 6</div> تصميم بنرين اسم مع التركيب لمدة <font color="#0000FF">شهرين </font> 
+			<strike><font face="Ebrima" color="#000000">200</font></strike>
+			<span lang="ar-ye">الى</span> <font face="Ebrima">150</font> ريال </p>
+	<p style="text-align: center"><b><u>ينتهي العرض 
+	<span lang="ar-ye">يوم الاثنين مساءا<font color="#0000FF">ً</font></span></u></font></b><p style="text-align: center">
+		<img src="https://www.te3b.com/dro3/z1bru37bva10.gif"><b><font color="#008000"><span lang="ar-ye">
+		</span> </font><span style="background-color: #FFFFFF">وكل عام </span><span lang="ar-ye">
+		<span style="background-color: #FFFFFF">وعموم الشعب العماني حكومه وشعباً ب الف خير</span> </span>
+		&nbsp;</b><p style="text-align: center"><u><b>
+	<a target="_blank" href="https://www.te3b.com/sico/z1dehro0e410.JPG">و للتواصل مع الاداره اضغط هنا
+	</a></b></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
