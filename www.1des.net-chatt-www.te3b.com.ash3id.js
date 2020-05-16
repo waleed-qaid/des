129d2108a4d27a65b59dfff0925dc32607122bc2
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2020-05-17 02:23:39 +0300

    Update www.1des.net-chatt-www.te3b.com.ash3id.js

--- www.1des.net-chatt-www.te3b.com.ash3id.js
+++ www.1des.net-chatt-www.te3b.com.ash3id.js
@@ -1,9 +1,9 @@
 $(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
 	<font id="sultan0" class=" sultan11"style="
     font-family: jazeera-light, FontAwesome;
-" >  <font color="#FF0000">  <u><b>بمناسبة <span lang="ar-sa">شهر رمضان</span> / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
+" >  <font color="#FF0000">  <u><b>بمناسبة <span lang="ar-sa">عيد الفطر</span> / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
 			<b><font color="#000080">عروض مخفضه للمشتركين / بمناسبة قرب
-			<span lang="ar-sa">شهر رمضان المبارك</span><span lang="ar-ye">
+			<span lang="ar-sa">عيد الفطر المبارك</span><span lang="ar-ye">
 			</span>&nbsp;</font></b></p></p><div style="float: right;padding-right: 20px;">-1</div> درع ماستر ذهبي لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
 			</span> </font><span lang="ar-ye">
 			<font face="Ebrima" color="#000000">
@@ -29,12 +29,13 @@ $(`<center><div style="position: static;width: 100%;" onclick="setTimeout(functi
 			<font face="Ebrima" color="#000000">500</font></strike></span><font color="#0000FF"> </font> 
 			<span lang="ar-ye">الى</span> <font face="Ebrima">300</font> ريال + سوبر اسود هديه <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">-5</div><img src="https://www.te3b.com/s3.gif"> تاج فضي لمدة <font color="#0000FF"> شهرين </font> 
 			<strike><font face="Ebrima" color="#000000">400</font></strike><font color="#0000FF"> </font>
-			<span lang="ar-ye">الى</span> <font face="Ebrima">200</font> ريال + سوبر فضي هديه &nbsp;<img src="https://www.te3b.com/sico/z1d3c137ha10.gif"></p><div style="float: right;padding-right: 20px;"> - 6</div> تصميم بنرين اسم مع التركيب لمدة <font color="#0000FF">شهرين </font> 
+			<span lang="ar-ye">الى</span> <font face="Ebrima">200</font> ريال + سوبر فضي هديه &nbsp;<img src="https://www.te3b.com/sico/z1d3c137ha10.gif"></p><div style="float: right;padding-right: 20px;"> - 6</div> تصميم بنرين اسم مع التركيب لمدة <font color="#0000FF">
+		شهر </font> 
 			<strike><font face="Ebrima" color="#000000">200</font></strike>
 			<span lang="ar-ye">الى</span> <font face="Ebrima">150</font> ريال </p>
 		<p style="text-align: center"><u><font color="#0000FF"><b>ملاحظه : </b>
 	</font></u><b><u><font color="#0000FF">ينتهي العرض 
-	<span lang="ar-sa">في 6 رمضان</span></font></u></font></b><p style="text-align: center">
+	<span lang="ar-sa">نهاية شهر رمضان</span></font></u></font></b><p style="text-align: center">
 		<span lang="ar-sa"><b>وكل عام والجميع ب الف خير وعافيه</b></span><p style="text-align: center"><u><b>
 	<a target="_blank" href="https://www.te3b.com/sico/z1dehro0e410.JPG">و للتواصل مع الاداره اضغط هنا
 	</a></b></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});
