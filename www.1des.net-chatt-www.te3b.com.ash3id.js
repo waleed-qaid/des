ec5431be3c360fb38552094a84afe9a778ccd8c5
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2020-09-20 00:28:32 +0300

    Update www.1des.net-chatt-www.te3b.com.ash3id.js

--- www.1des.net-chatt-www.te3b.com.ash3id.js
+++ www.1des.net-chatt-www.te3b.com.ash3id.js
@@ -1,9 +1,10 @@
-$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #da0a0a;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
-	<font id="sultan0" class=" sultan11"style="
-    font-family: jazeera-light, FontAwesome; >  
-	<font color="#b21f18">  <u><b>بمناسبة <span lang="ar-sa">عيد الاضحى</span> / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
-			<b><font color="#000080">عروض مخفضه للمشتركين / بمناسبة قرب
-			<span lang="ar-sa">عيد الاضحى المبارك</span><span lang="ar-ye">
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
+	<img src="https://www.te3b.com/dro3/z1bru3sa1h10.gif"><font id="sultan" class=" sultan11"><u><b><font color="#008000"><span lang="ar-ye"> 
+	بمناسبة اليوم الوطني</span> / اضغط  هنا</font> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
+			<b><font color="#003C00"> <span lang="ar-ye">تخفيضات</span> خاصه 
+			للمشتركين / بمناسبة قـُرب </font><span lang="ar-ye">
+			<font color="#003C00"> اليوم الوطني السعودي</font><font color="#000080"> </font>
+			</span></b><img src="https://www.te3b.com/sico/z1cijg4dgk10.gif"><b><font color="#000080"><span lang="ar-ye">
 			</span>&nbsp;</font></b></p></p><div style="float: right;padding-right: 20px;">-1</div> درع ماستر ذهبي لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
 			</span> </font><span lang="ar-ye">
 			<font face="Ebrima" color="#000000">
@@ -13,10 +14,11 @@ $(`<center><div style="position: static;width: 100%;" onclick="setTimeout(functi
 			</span> </font> <span lang="ar-ye"><font face="Ebrima"><strike>
 			<font color="#000000"> 1200</font></strike></font></span><font color="#008080"> </font> 
 			<span lang="ar-ye">الى</span> <span lang="ar-ye">
-		<font face="Ebrima">850</font></span> <span lang="ar-ye">/ </span>مع اللوحه &nbsp;<img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">-2</div> درع ماستر اسود لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
+			<font face="Ebrima">850</font></span> <span lang="ar-ye">/ </span>مع اللوحه &nbsp;<img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p><div style="float: right;padding-right: 20px;">-2</div> درع ماستر اسود لمدة <font color="#0000FF"> شهرين<span lang="ar-ye">
 			</span> </font><span lang="ar-ye"><strike>
 			<font face="Ebrima" color="#000000">1300</font></strike></span><font color="#0000FF"> </font>
-			<span lang="ar-ye">الى</span> <font face="Ebrima">1000</font> /<font color="#008080"> مع اضافة لايكات و ريش </font><img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات و ريش<span lang="ar-ye">
+			<span lang="ar-ye">الى</span> <span lang="ar-ye">
+		<font face="Ebrima">1000</font></span> /<font color="#008080"> مع اضافة لايكات و ريش </font><img src="https://www.te3b.com/sico/z1bpqscbf510.gif"></p><div style="float: right;padding-right: 20px;">- -</div><font color="#008080"> اما بدون اضافة لايكات و ريش<span lang="ar-ye">
 			</span> </font> <span lang="ar-ye"><strike>
 			<font face="Ebrima" color="#000000">1000</font></strike></span><font color="#008080"> </font> 
 			<span lang="ar-ye">الى</span> <span lang="ar-ye">
@@ -33,8 +35,10 @@ $(`<center><div style="position: static;width: 100%;" onclick="setTimeout(functi
 		شهر </font> 
 			<strike><font face="Ebrima" color="#000000">200</font></strike>
 			<span lang="ar-ye">الى</span> <font face="Ebrima">150</font> ريال </p>
-		<p style="text-align: center"><b><u><font color="#0000FF">ينتهي العرض 
-		اول ايام عيد الاضحى المبارك</font></u></font></b><p style="text-align: center">
-		<span lang="ar-sa"><b>وكل عام والجميع ب الف خير وعافيه / الاداره</b></span><p style="text-align: center"><u><b>
+	<p style="text-align: center"><b><u><font color="#0000FF">ينتهي العرض 
+	<span lang="ar-ye">يوم الاربعاء مساءاً</span></font></u></font></b><p style="text-align: center">
+		<img src="https://www.te3b.com/dro3/z1bru38ccg10.png"><b><font color="#008000"><span lang="ar-ye">
+		</span>وكل عام </font><span lang="ar-ye"><font color="#008000">وعموم 
+		الشعب السعودي حكومه وشعباً ب الف خير</font> </span>&nbsp;</b><p style="text-align: center"><u><b>
 	<a target="_blank" href="https://www.te3b.com/sico/z1dehro0e410.JPG">و للتواصل مع الاداره اضغط هنا
 	</a></b></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});
