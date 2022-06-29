7005327d383239a6c32b62ee801f0e8d3661c234
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2022-06-29 16:57:45 +0300

    Create www.1des.net-chatt-www.te3b.com.ash3id.js

--- www.1des.net-chatt-www.te3b.com.ash3id.js
+++ www.1des.net-chatt-www.te3b.com.ash3id.js
@@ -1,1 +1,54 @@
-$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( المخفي ) + ( سوبر فضي هديه ) بـ 600 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d1hdjuci10.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( المخفي ) بـ 500 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c137ha10.gif"> </p><div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال لمدة : شهرين <img src="https://www.te3b.com/s1.gif"></p><div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 150 ريال لمدة : شهر <img src="https://www.te3b.com/s2.gif"></p><div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 100 ريال لمدة : شهر <img src="https://www.te3b.com/s3.gif"><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</font></b><p style="text-align: center"><u><b><a target="_blank" href="https://www.te3b.com/sico/z1f5c4o6s610.JPG">وللتواصل مع الاداره اضغط هنا</a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$(`<style>.nosel.d-flex.room.r180b31492f9x922d0131xn7fcg1 .fitimg.u-pic.borderg{border-radius: 100px;border: 2px solid #ff7ba7;padding-top:30px;}.nosel.d-flex.room.r180b31492f9x922d0131xn7fcg1 .corner.fa.fa-user.label.uc.fa-microphone.label-danger {text-transform: uppercase;letter-spacing:1px;background-image: linear-gradient(-225deg, #231557 0%, #511559 29%, #ff1361 67%, #231557 100%);background-size: auto auto;background-clip: border-box;background-size: 200% auto;color: #fff;text-fill-color: white;-webkit-background-clip: text;-webkit-text-fill-color: #ffffff00;animation: textclip 2s linear infinite;display: inline-block;margin-top: -3px!important;}.nosel.d-flex.room.r180b31492f9x922d0131xn7fcg1 .u-topic.dots {text-transform: uppercase;background-image: linear-gradient(-225deg, #231557 0%, #44107a 29%, #ff1361 67%, #3600ff 100%);background-size: auto auto;background-clip: border-box;background-size: 200% auto;color: #fff;background-clip: text;text-fill-color: transparent;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 2s linear infinite;display: inline-block;font-size: 190px;}@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" >
+	<font id="sultan0" class=" sultan11"style="
+    font-family: jazeera-light, FontAwesome; background-color:#000000" >  
+	<font color="#FFFFCC">  <u><b>بمناسبة عيد الاضحى / اضغط هنا</b></u></font></font><font id="sultan" class=" sultan11"><u><b> </b></u></font><div id="spanan"style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;"><p style="text-align: center">
+			<b><font color="#000080">عروض مخفضه للمشتركين / بمناسبة قرب
+			<span lang="ar-sa">عيد الاضحى المبارك</span><span lang="ar-ye">
+			</span>&nbsp;</font></b></p></p><div style="float: right;padding-right: 20px;">-1</div>  
+		<img src="https://khleeg.us/sico/z1ffngr0mj10.gif"><img src="https://alriad.net/emo/z1g6nqm6ci1019amry0XiPfn.gif"> 
+		اشترك ادمن ذهبي <font color="#0000FF">شهرين </font><b> <font face="Ebrima">1300</font></b> 
+		واحصل ع<font color="#008000"> </font><b><font color="#000000">(ادمن اسود 
+		وتاج ذهبي)</font></b><font color="#008000"> هديه</font>
+		<font color="#008080"> <span lang="ar-ye"> &nbsp;</span>&nbsp;</font><img src="https://www.te3b.com/sico/z1c1ib2ng710.gif"></p>
+		<div style="float: right;padding-right: 20px; width:34px; height:19px">
+			2</div> <img src="https://www.te3b.com/s2.gif"><img src="https://www.te3b.com/sico/z1d3c130e710.GIF"> اشترك ادمن اسود
+		<font color="#0000FF">شهرين </font><b><font face="Ebrima">1100</font></b> 
+		واحصل ع<font color="#008000"> </font><font color="#000000"><b>(</b></font><b><font color="#000000">تاج 
+		اسود ونجمه)</font></b><font color="#008000"> هديه</font>
+		<font color="#008080"> <span lang="ar-ye"> &nbsp;</span>&nbsp;</font><img src="https://alriad.net/emo/z1g6nqm6ci1019amry0XiPfn.gif"></p><div style="float: right;padding-right: 20px;">-3</div><img src="https://www.te3b.com/s1.gif"><img src="https://www.te3b.com/s1.gif"> 
+		اشترك تاج ذهبي <font color="#008080"><u> مخفي </u></font>
+		<font color="#0000FF"> شهرين<span lang="ar-ye">
+			</span> </font><b><font face="Ebrima">500 </font></b>
+		<span lang="ar-ye">واحصل ع <b><font color="#000000">(نجمتين ذهبيه)</font></b></span>
+		<font color="#008000">هديه</font>  
+		<img src="https://khleeg.us/sico/z1ffngr0mj10.gif"></p><div style="float: right;padding-right: 20px;">-4</div><img src="https://www.te3b.com/s2.gif"><img src="https://www.te3b.com/s2.gif"> 
+		اشترك تاج اسود <u><font color="#008080"> مخفي </font></u>
+		<font color="#0000FF"> &nbsp;شهرين<span lang="ar-ye">
+			</span> </font><b><font face="Ebrima">400</font></b><font color="#0000FF"> </font>
+		واحصل ع <font color="#000000"><b>(نجمتين سودا)</b></font>
+		<font color="#008000">هديه</font> <img src="https://www.te3b.com/sico/z1d3c130e710.GIF"></p><div style="float: right;padding-right: 20px;">-5</div><img src="https://www.te3b.com/s3.gif"><img src="https://www.te3b.com/s3.gif"> 
+		اشترك تاج فضي&nbsp; <font color="#0000FF"> شهرين </font><b>
+		<font face="Ebrima">300</font></b><font color="#0000FF"> </font>واحصل ع
+		<b><font color="#000000">(نجمتين فضيه)</font></b> <font color="#008000">هديه</font> &nbsp;<img src="https://www.te3b.com/sico/z1d3c137ha10.gif"></p><div style="float: right;padding-right: 20px;">
+			-6</div>&nbsp;اشترك <b>( عشره نجوم ذهبيه )</b> <b>
+		<font color="#0000FF">شهر</font></b> <b><font face="Ebrima">900</font></b> 
+		واحصل ع <b><font color="#000000">(روم خاص)</font></b>
+		<font color="#008000">هديه</font> <img src="https://www.te3b.com/s1.gif"><img src="https://www.te3b.com/s1.gif"></p><div style="float: right;padding-right: 20px;">
+			-7</div>&nbsp;اشترك <b>( عشره نجوم سودا )</b> <b>
+		<font color="#0000FF">شهر</font></b> <b><font face="Ebrima">750</font></b> 
+		واحصل ع <b><font color="#000000">( بنــــــر )</font></b>
+		<font color="#008000">هديه</font> <img src="https://www.te3b.com/s2.gif"><img src="https://www.te3b.com/s2.gif"></p><div style="float: right;padding-right: 20px;"> 
+			- 8</div> <img src="https://lll6.net/sico/z1fsbhp8el10q5CP5G0v9u.gif">&nbsp;اشترك <b>( 
+		روم صوتي )</b> <font color="#008000"><b>شهريييين</b></font> <b>
+		<font face="Ebrima" color="#008000">500</font></b> واحصل ع <b><font color="#008000">
+		(تاج خاص بالروم) ـ</font></b>&nbsp;&nbsp;<b>
+		<img src="https://lll6.net/sico/z1fsbhp8el10q5CP5G0v9u.gif"></p><div style="float: right;padding-right: 20px;"> 
+			- 9</div> تصميم بنرين&nbsp; اسم مع التركيب&nbsp; <b> <font color="#0000FF">شهر </font>
+		<font face="Ebrima">150</font></b><font color="#0000FF"> </font>&nbsp;</p>
+		</p>
+		<p style="text-align: center"><u><font color="#0000FF">ملاحظه : ينتهي العرض 
+		اول ايام عيد الاضحى المبارك</font></u></font><p style="text-align: center">
+		<span lang="ar-sa"><b>وكل عام والجميع ب الف خير وعافيه</b></span><p style="text-align: center"><u>
+		<a target="_blank" href="https://te3b.com/sico/z1f5c4o6s610.JPG">و للتواصل مع الاداره اضغط هنا
+		</a></u></font></div></div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){})});
